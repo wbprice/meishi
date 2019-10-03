c48114527c0110a9fcf9fd6fe9550f83770707b9
Blaine Price <1wbprice@gmail.com>
2019-10-03 12:51:55 -0400

    Permission issue when trying to read object in bucket.

--- read-business-card/main.go
+++ read-business-card/main.go
@@ -10,6 +10,7 @@ import (
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/aws/aws-sdk-go/service/textract"
 )
 
@@ -26,11 +27,31 @@ func handler(ctx context.Context, s3Event events.S3Event) {
 	// Configure the Textract client
 	textractClient := textract.New(session)
 
+	// Configure the S3 client
+	s3Client := s3.New(session)
+
 	// Iterate over file upload events
 	for i := 0; i < len(s3Event.Records); i++ {
 		record := s3Event.Records[i]
-		fmt.Printf(record.EventName)
-		fmt.Printf(record.S3.Object.Key)
+		fmt.Println(record.EventName)
+		fmt.Println(record.S3.Object.Key)
+		fmt.Println(s3BucketName)
+
+		// Get file bytes
+		blobInput := &s3.GetObjectInput{
+			Bucket: aws.String(s3BucketName),
+			Key:    aws.String(record.S3.Object.Key),
+		}
+
+		blob, err := s3Client.GetObject(blobInput)
+
+		if err != nil {
+			fmt.Println("Something went wrong fetching the file")
+			log.Fatal(err)
+		} else {
+			fmt.Println("The blob")
+			fmt.Println(blob)
+		}
 
 		s3Object := textract.S3Object{
 			Bucket: &s3BucketName,
@@ -38,7 +59,7 @@ func handler(ctx context.Context, s3Event events.S3Event) {
 		}
 		document := textract.Document{
 			S3Object: &s3Object,
-			Bytes:    []byte{},
+			// Bytes:    []byte{},
 		}
 
 		fmt.Printf("Hello %s\n", *document.S3Object.Bucket)
